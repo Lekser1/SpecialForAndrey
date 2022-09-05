05dcec1c454fb5c3aa26708c924f381ab3ec4425
LeonidL <leonidloktik3@yandex.ru>
2022-09-05 20:45:37 +0300

    Initial Commit

---
+++ NewTrain/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  NewTrain
+//
+//  Created by Леонид Локтик on 05.09.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
