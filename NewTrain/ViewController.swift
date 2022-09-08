e94f16f994221698e636e99733ab8e6264346c24
LeonidL <leonidloktik3@yandex.ru>
2022-09-08 21:31:22 +0300

    Специально для Андрюхи

--- NewTrain/ViewController.swift
+++ NewTrain/ViewController.swift
@@ -8,10 +8,38 @@
 import UIKit
 
 class ViewController: UIViewController {
+    
+    var nextButton = UIButton()
+    var newImageView = UIImageView()
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        self.title = "HALOOO"
+        
+        self.nextButton = UIButton(type: .system)
+        self.nextButton.setTitle("Go through", for: .normal)
+        nextButton.center = view.center
+        //self.nextButton.sizeToFit()
+        nextButton.addTarget(self, action: #selector(goThrough), for: .touchUpInside)
+        self.view.addSubview(nextButton)
+        
+        newImageView = UIImageView(frame: CGRect(x: 0, y: 0, width: 50, height: 50))
+        if let image = UIImage(named: "apple") {
+            newImageView.image = image
+            newImageView.contentMode = .scaleAspectFit
+            self.navigationItem.titleView = newImageView
+        }
+        
+        self.navigationItem.rightBarButtonItem = UIBarButtonItem(barButtonSystemItem: .add, target: self, action: #selector (goThrough))
+        
+    }
+    
+    
+    
+    @objc func goThrough() {
+        let vc2 = ViewController2()
+        self.navigationController?.pushViewController(vc2, animated: true)
+        
     }
 
 
